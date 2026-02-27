45454165554e16d8920f0c119f80f36b66ffbd16
Denis <Denis@gmail.com>
2026-02-27 14:07:43 +0300

    исправил папку app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -6,17 +6,18 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider } from 'react-router'
 import { ROUTES } from './routes/routes.jsx'
 
-import './index.css'
+import "./styles/index.css";
 
-const queryClient = new QueryClient()
+const container = document.getElementById("app");
+const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')).render(
+// Исправление: используем createRoot напрямую, а не ReactDOM.createRoot
+createRoot(container).render(
   <StrictMode>
-    <MantineProvider >
+    <MantineProvider>
       <QueryClientProvider client={queryClient}>
-        <RouterProvider router={ROUTES}/>
+        <RouterProvider router={ROUTES} />
       </QueryClientProvider>
     </MantineProvider>
   </StrictMode>,
-)
-
+);
